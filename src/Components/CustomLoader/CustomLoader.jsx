c0ee2ac7ec3c2e9f0a01c3fed1285ab6ddfa675e
[info.qwt@gmail.com] <divyansh@wellm.co>
2024-05-05 14:15:54 +0530

    solving bugs while infinite scroll

---
+++ src/Components/CustomLoader/CustomLoader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CustomLoader.css";
+
+const CustomLoader = () => {
+  return <div id="loadingdata"></div>;
+};
+
+export default CustomLoader;
--- src/Components/JobListCard/JobListCard.jsx
+++ src/Components/JobListCard/JobListCard.jsx
@@ -1,20 +1,171 @@
-import React, { useEffect } from "react";
+// import React, { useEffect, useState } from "react";
+// import "./JobListCard.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchJobData } from "../../Redux/JobListCardSlicer";
+// import CustomLoader from "../CustomLoader/CustomLoader";
+
+// const JobListCard = () => {
+//   const dispatch = useDispatch();
+//   const { isLoading, data } = useSelector((state) => state.jobListCard);
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   useEffect(() => {
+//     dispatch(fetchJobData());
+//   }, [dispatch]);
+//   console.log(data);
+
+//   useEffect(() => {
+//     if (!isLoading) {
+//       setIsLoaded(true);
+//     }
+//   }, [isLoading]);
+//   return (
+//     <>
+//       {isLoading ? (
+//         <CustomLoader />
+//       ) : (
+//         data.jdList &&
+//         data.jdList.map((jobCard) => {
+//           if (
+//             !jobCard.companyName ||
+//             !jobCard.jobRole ||
+//             !jobCard.location ||
+//             !jobCard.minJdSalary ||
+//             !jobCard.maxJdSalary ||
+//             !jobCard.jobDetailsFromCompany ||
+//             !jobCard.jdLink ||
+//             !jobCard.minExp
+//           ) {
+//             // Skip rendering the job card if any detail is empty
+//             return null;
+//           }
+//           return (
+//             <div
+//               className={`job-list-card ${isLoaded ? "loaded" : ""}`}
+//               key={jobCard.id}
+//             >
+//               <div className="job-posted-how-much-ago">
+//                 <div className="job-posted-time-block">
+//                   <p>⏳ Posted 15 days ago</p>
+//                 </div>
+//               </div>
+//               <div className="full-job-details">
+//                 <div className="logo-with-company-name">
+//                   <img src="" alt="" />
+//                   <div>
+//                     <div className="company-info">
+//                       <h3>{jobCard.companyName}</h3>
+//                       <h2>{jobCard.jobRole}</h2>
+//                     </div>
+//                     <p className="job-location">{jobCard.location}</p>
+//                   </div>
+//                 </div>
+//                 <p className="estimated-salary">
+//                   Estimated Salary: ₹{jobCard.minJdSalary} - ₹
+//                   {jobCard.maxJdSalary} LPA{" "}
+//                   <span aria-label="Offered salary range" class="">
+//                     {" "}
+//                     ✅
+//                   </span>
+//                   <br />
+//                 </p>
+//                 <div className="about-company-content">
+//                   <div>
+//                     <p>About Company:</p>
+//                     <div className="about-company-whole-content">
+//                       <p>
+//                         <strong>About us</strong>
+//                       </p>
+//                       <p>
+//                         <span className="fw-400">
+//                           {jobCard.jobDetailsFromCompany}
+//                         </span>
+//                       </p>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="view-job-link">
+//                   <a href={jobCard.jdLink} className="fw-400">
+//                     View job
+//                   </a>
+//                 </div>
+//                 <div className="experience-required">
+//                   <h3>Minimum Experience</h3>
+//                   <h2>{jobCard.minExp} years</h2>
+//                 </div>
+//               </div>
+//               <div className="easy-apply-button-container">
+//                 <div className="easy-apply-button-block">
+//                   <button>⚡ Easy Apply</button>
+//                 </div>
+//               </div>
+//               <div className="unlock-referral-button">
+//                 <button>Unlock referral asks</button>
+//               </div>
+//             </div>
+//           );
+//         })
+//       )}
+//     </>
+//   );
+// };
+
+// export default JobListCard;
+
+import React, { useEffect, useState } from "react";
 import "./JobListCard.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchJobData } from "../../Redux/JobListCardSlicer";
+import CustomLoader from "../CustomLoader/CustomLoader";
 
 const JobListCard = () => {
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => state.jobListCard);
+  const { isLoading, data } = useSelector((state) => state.jobListCard);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isFetching, setIsFetching] = useState(false); // Track if data fetching is in progress
+  const [offset, setOffset] = useState(0); // T
+
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        window.innerHeight + document.documentElement.scrollTop !==
+          document.documentElement.offsetHeight ||
+        isLoading ||
+        isFetching
+      )
+        return;
+      setIsFetching(true);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isLoading, isFetching]);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    fetchMoreData();
+  }, [isFetching]);
+
+  const fetchMoreData = () => {
+    dispatch(fetchJobData({ offset: offset + 10 })); // Fetch more data with an increased offset
+    setOffset(offset + 10); // Update offset
+    setIsFetching(false); // Reset isFetching state
+  };
 
   useEffect(() => {
     dispatch(fetchJobData());
   }, [dispatch]);
   console.log(data);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setIsLoaded(true);
+    }
+  }, [isLoading]);
   return (
     <>
       {isLoading ? (
-        <h1>loading...</h1>
+        <CustomLoader />
       ) : (
         data.jdList &&
         data.jdList.map((jobCard) => {
@@ -32,7 +183,10 @@ const JobListCard = () => {
             return null;
           }
           return (
-            <div className="job-list-card">
+            <div
+              className={`job-list-card ${isLoaded ? "loaded" : ""}`}
+              key={jobCard.id}
+            >
               <div className="job-posted-how-much-ago">
                 <div className="job-posted-time-block">
                   <p>⏳ Posted 15 days ago</p>
